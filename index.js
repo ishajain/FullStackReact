a6ace2f6814a4d9832b2a146c98bdc255908eccb
Isha Jain <isha.jain30@gmail.com>
2019-03-28 16:04:27 +1100

    Changed Greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express(); // this create the express application server
 // Create Route Handler
 app.get("/", (request, response) => {
-  response.send({ hi: "there" });
+  response.send({ hi: "First Heroku app" });
 });
 
 const PORT = process.env.PORT || 5000;
