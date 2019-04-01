f0fe7cf85fc46c36ac2c56cc7210c5dc373f7c6e
Isha Jain <isha.jain30@gmail.com>
2019-04-01 17:40:45 +1100

    Committed Keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+console.log("\x1b[36m%s\x1b[0m", process.env.Node_ENV);
+
 if (process.env.Node_ENV === "production") {
   module.exports = require("../config/prod");
   console.log("\x1b[36m%s\x1b[0m", process.env.Node_ENV);
